78a8a410345d30f33bf3c57502f85f27cc4c8591
mredivo9 <168040123+mredivo9@users.noreply.github.com>
2024-09-08 21:06:20 -0300

    Add files via upload

---
+++ Vetores.java
@@ -0,0 +1,7 @@
+public class Vetores {
+    public static void main(String[] args) {
+        int[] vetor = {10, 20, 30, 40, 50};
+
+        System.out.println("O terceiro elemento do vetor é: " + vetor[2]);
+    }
+}
